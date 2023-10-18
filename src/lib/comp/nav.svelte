1812118cf0a7b1a82231d6e67fcff29de6a940b6
Joshua Lopes <joshrl@proton.me>
2023-10-18 21:10:00 +0100

    fixed nav alignment

--- src/lib/comp/nav.svelte
+++ src/lib/comp/nav.svelte
@@ -1,23 +1,26 @@
 <script>
-	import { Github, Globe, Rss } from 'lucide-svelte';
+	import { Github, Globe, Rss, Code2 } from 'lucide-svelte';
 </script>
 
 <nav
-	class="max-w-[999em] h-12 h-10 bg-zinc-100/30 dark:bg-zinc-900/30 backdrop-blur-lg sticky top-0 prose dark:prose-invert prose-a:text-xl prose-a:no-underline prose-a:font-bold prose-a:mx-4 prose-a:mt-4 prose-a:decoration-2 prose-a:cursor-pointer"
+	class="max-w-[999em] h-12 h-fit pb-4 bg-zinc-100/30 dark:bg-zinc-900/30 backdrop-blur-lg sticky top-0 prose dark:prose-invert prose-a:text-xl prose-a:no-underline prose-a:font-bold prose-a:mx-4 prose-a:mt-4 prose-a:decoration-2 prose-a:cursor-pointer"
 >
 	<div class="h-3" />
 	<div class="mx-5">
-		<div class="inline">
-			<a class="hover:underline" href="/">/</a>
-			<a class="hover:underline" href="/posts/">posts</a>
+		<div class="inline-block">
+			<a class="hover:underline inline-block" href="/">/</a>
+			<a class="hover:underline inline-block" href="/posts/">posts</a>
 		</div>
 		<div class="ml-auto flex float-right">
 			<a href="https://github.com/joshua861"
-				><Github class="hover:stroke-sky-500 transition-all" /></a
+				><Github class="hidden sm:inline hover:stroke-sky-500 transition-all" /></a
 			>
 			<a href="https://joshua861.github.io"><Globe class="hover:stroke-sky-500 transition-all" /></a
 			>
 			<a href="/rss/"><Rss class="hover:stroke-sky-500 transition-all" /></a>
+			<a class="hidden md:inline" href="https://github.com/joshua861"
+				><Code2 class="hover:stroke-sky-500 transition-all" /></a
+			>
 		</div>
 	</div>
 	<div class="h-3" />
