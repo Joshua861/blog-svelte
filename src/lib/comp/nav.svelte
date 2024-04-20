e0dbff2517fdd1a6576898bbef06790a9da47698
Joshua861 <joshrl@proton.me>
2024-04-20 21:58:36 +0100

    search

--- src/lib/comp/nav.svelte
+++ src/lib/comp/nav.svelte
@@ -1,9 +1,30 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
-	import { Github, Globe, Rss, Code2, Menu, X } from 'lucide-svelte';
+	import Fuse from 'fuse.js';
+	import { Github, Globe, Rss, Code2, Menu, Search, X } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 	import { slide } from 'svelte/transition';
 
 	let showMenu = false;
+	let searchMenu = false;
+	let search: string = '';
+	let posts, fuse: any, results;
+
+	onMount(async () => {
+		const response = await fetch(`/api/posts`);
+		posts = await response.json();
+		console.log(posts);
+		fuse = new Fuse(posts, { keys: ['meta.title', 'meta.description', 'path'] });
+	});
+
+	function updateSearch() {
+		results = fuse.search(search).slice(0, 5);
+		console.log(results);
+	}
+
+	function doNothing() {
+		event.stopPropagation();
+	}
 
 	let links = [
 		{
@@ -56,16 +77,37 @@
 					<svelte:component this={link.icon} class="hover:stroke-sky-500 transition-all" />
 				</a>
 			{/each}
+			<a title="Search posts">
+				<button
+					on:click={() => {
+						searchMenu = !searchMenu;
+						updateSearch();
+					}}
+				>
+					<Search class="hover:stroke-sky-500 transition-all" />
+				</button>
+			</a>
 		</div>
-		<button class="flex sm:hidden ml-auto float-right" on:click={() => (showMenu = !showMenu)}>
-			<Menu class="my-4 mx-5 hover:stroke-sky-500 transition-all" />
-		</button>
+		<div class="flex sm:hidden ml-auto float-right">
+			<button on:click={() => (showMenu = !showMenu)}>
+				<Menu class="my-4 mx-5 hover:stroke-sky-500 transition-all" />
+			</button>
+			<button
+				title="Search posts"
+				on:click={() => {
+					searchMenu = !searchMenu;
+					updateSearch();
+				}}
+			>
+				<Search class="hover:stroke-sky-500 transition-all" />
+			</button>
+		</div>
 	</div>
 	<div class="h-3" />
 
 	{#if showMenu}
 		<div
-			class="rounded-2xl h-fit dark:bg-zinc-800 bg-zinc-200 m-3 p-5 prose dark:prose-invert"
+			class="rounded-2xl h-fit dark:bg-zinc-800/50 bg-zinc-200/50 m-3 p-5 prose dark:prose-invert"
 			transition:slide
 		>
 			{#each links as link}
@@ -79,3 +121,53 @@
 		</div>
 	{/if}
 </nav>
+
+{#if searchMenu}
+	<div
+		on:click={() => {
+			searchMenu = false;
+		}}
+		class="w-screen h-screen absolute top-0 backdrop-blur-md z-20 flex justify-center align-center"
+	>
+		<div
+			on:click={doNothing}
+			class="my-auto p-3 rounded-2xl prose dark:prose-invert w-full max-w-[480px] dark:bg-[#1E1E22] bg-[#EBEBF0] mx-5 z-30"
+		>
+			<div class="flex">
+				<input
+					on:input={updateSearch}
+					bind:value={search}
+					type="text"
+					class="inline w-max inp flex-1"
+					placeholder="Search posts"
+					autofocus
+				/>
+				<button on:click={() => (searchMenu = false)}>
+					<X
+						class="opacity-50 my-auto ml-3 hover:stroke-sky-500 transition-all hover:opacity-100"
+					/>
+				</button>
+			</div>
+			{#if results.length !== 0}
+				<div class="flex flex-col gap-3 pt-3">
+					{#each results as post}
+						<a
+							class="no-underline hover:dark:text-zinc-100 hover:text-zinc-900"
+							href={post.item.path}
+						>
+							<div
+								class="bg-zinc-300 dark:bg-zinc-800 rounded-xl hover:bg-sky-300/20 transition-all dark:hover:bg-sky-700/20 p-3"
+							>
+								<strong>{post.item.meta.title}</strong>
+								<br />
+								<span class="text-zinc-500 text-sm">
+									{post.item.meta.description}
+								</span>
+							</div>
+						</a>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</div>
+{/if}
