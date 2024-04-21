850ed0e6f0a10d0d5139b0a383ffd42e433f001d
Joshua861 <joshrl@proton.me>
2024-04-21 08:45:38 +0100

    back to top button

---
+++ src/lib/comp/scroll-to-top.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { ChevronUp } from 'lucide-svelte';
+
+	let showButton = false;
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			showButton = true;
+		} else {
+			showButton = false;
+		}
+	}
+
+	function topFunction() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+</script>
+
+<button
+	on:click={topFunction}
+	class="fixed bottom-4 right-4 p-2 bg-sky-500/20 dark:text-white text-black rounded-full backdrop-blur-md"
+	style="display: {showButton ? 'block' : 'none'}"
+>
+	<ChevronUp />
+</button>
+
+<svelte:window on:scroll={scrollFunction} />
--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,16 +1,19 @@
 <script>
-	import { page } from "$app/stores";
-  import Commentbox from "$lib/comp/commentbox.svelte"
-  import Comments from "$lib/comp/comments.svelte";
+	import { page } from '$app/stores';
+	import Commentbox from '$lib/comp/commentbox.svelte';
+	import Comments from '$lib/comp/comments.svelte';
+	import ScrollToTop from '$lib/comp/scroll-to-top.svelte';
 
-  $: pathname = $page.url.pathname
+	$: pathname = $page.url.pathname;
 </script>
 
-<slot></slot>
+<slot />
 
 {#if pathname !== '/posts'}
-<hr /><div />
-<Commentbox {pathname} />
-<br><br>
-<Comments {pathname} />
+	<hr />
+	<div />
+	<Commentbox {pathname} />
+	<br /><br />
+	<Comments {pathname} />
+	<ScrollToTop />
 {/if}
